8fcf596cfcfc77046fba1ba68dd7c74318db46a3
Md-Alqma <mdalqma7@gmail.com>
2025-04-23 16:30:29 +0530

    created navbar and hero components

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
 import "./App.css";
+import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <nav className="flex justify-between text-white items-center overflow-hidden">
-        <div>
-          <img src="/images/logo.png" alt="logo" />
-        </div>
-
-        <div className="flex gap-4 items-center">
-          <button
-            className="relative px-4 py-2 border border-gray-500 font-semibold rounded-lg bg-no-repeat bg-center bg-cover drop-shadow-sm"
-            style={{
-              backgroundImage: `url(/images/button-1.png'), url('/images/button-2.png')`,
-              backgroundPosition: "left center, right center",
-              backgroundSize: "80px 80px, 80px 80px",
-              backgroundRepeat: "no-repeat, no-repeat",
-            }}
-          >
-            Enquire Now
-          </button>
-          <div>Menu</div>
-        </div>
-      </nav>
+      <Navbar />
       <main>
-        <div
-          className="py-10 mt-6"
-          style={{
-            backgroundImage: `url(/images/hero.jpg)`,
-            backgroundPosition: "center center",
-            backgroundSize: "720px 100%",
-            backgroundRepeat: "no-repeat",
-            backgroundBlendMode: "color-burn",
-          }}
-        >
-          <div className="ml-25">
-            <h1 className="text-[9rem]">DESIGN,</h1>
-          </div>
-          <div className="ml-[30rem]">
-            <h1 className="text-[9rem]">DEVELOP,</h1>
-          </div>
-          <div className="ml-25">
-            <h1 className="text-[9rem]">RESOLVE.</h1>
-          </div>
-        </div>
+        <Hero />
       </main>
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-between text-white items-center overflow-hidden">
+      <div>
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+
+      <div className="flex gap-8 items-center">
+        <div className="relative overflow-hidden rounded-lg">
+          <img
+            className="w-24 h-32 absolute -left-[20px] top-1/2 -translate-y-1/2"
+            src="/images/button-1.png"
+            alt=""
+          />
+          <button className="relative px-4 py-2 font-semibold rounded-lg outline-none z-99 cursor-pointer">
+            Enquire Now
+          </button>
+          <img
+            className="w-24 h-32 absolute  -right-[20px] top-1/2 -translate-y-1/2"
+            src="/images/button-1.png"
+            alt=""
+          />
+        </div>
+
+        <div>Menu</div>
+      </div>
+    </nav>
+  );
+};
